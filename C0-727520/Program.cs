466d5eadbe9216d984ca774de528e26a43498cab
visualstudio <visualstudio@230-PC32>
2019-01-31 16:59:49 -0500

    commit at 4:59

--- C0-727520/Program.cs
+++ C0-727520/Program.cs
@@ -4,55 +4,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0727520_week4
+namespace C0727520_week4
 {
     class Program
     {
         static void Main(string[] args)
         {
-        }
-        class Village
-        {
-            public static int numberOfVillages = 0;
-            public Village nextVillage;
-            public Village previousVillage;
-            public string VillageName;
-            public bool isAstrildeHere = false;
+            Countryside Ontario = new Countryside();
+            Ontario.run();
 
-            public Village()
-            {
-                Village.numberOfVillages++;
-            }
-        }
-        class Countryside
-        {
-            public Village Maple;
-            public Village Toronto;
-            public Village Ajax;
-
-            public void MapInitializer()
-            {
-                Maple = new Village();
-                Maple.VillageName = "Maple";
-                Maple.previousVillage = null;
-                Maple.nextVillage = Toronto;
-                Toronto = new Village();
-                Toronto.previousVillage = Maple;
-
-
-                Toronto.VillageName = "Toronto";
-                Toronto.nextVillage = Ajax;
-                Ajax = new Village();
-                Ajax.VillageName = "Ajax";
-                Ajax.VillageName = null;
-                Ajax.previousVillage = Toronto;
-            }
-        }
-        public void LookForAstrilde()
-        {
-            //gg
         }
     }
+
+    class LearningExample
+    {
+        public void run()
+        {
+            Village Toronto;
+            Village a;
+            Village b;
+            Toronto = new Village();
+            a = Toronto;
+
+            Toronto = new Village();
+            b = Toronto;
+
+            if (a == b) { Console.WriteLine("same"); }
+            else
+            {
+                Console.WriteLine("different obj refs");
+                Console.ReadLine();
+            }
+        }
+    }
+    class Village
+    {
+        public static int numberOfVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        public Village()
+        {
+            Village.numberOfVillages++;
+        }
+    }
+
+    class Countryside
+    {
+        public Village Maple;
+        public Village Toronto;
+        public Village Ajax;
+        public Village Head;
+        public Village Tail;
+        public Village Current;
+
+        public void run()
+        {
+            this.MapInitializer();
+            this.LookForAstrilde();
+            Console.WriteLine("Hugi found Astrilde in " + Current.VillageName);
+            Console.ReadLine();
+        }
+
+        public void MapInitializer()
+        {
+            Ajax = new Village();
+            Toronto = new Village();
+            Maple = new Village();
+
+            Maple.VillageName = "Maple";
+            Maple.previousVillage = null;
+            Maple.nextVillage = Toronto;
+            Ajax.isAstrildeHere = true;
+
+            Toronto.previousVillage = Maple;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+
+        }
+
+
+        public void LookForAstrilde()
+        {
+            Current = Maple;
+            while (Current.nextVillage != null)
+            {
+                if (Current.isAstrildeHere)
+                {
+                    Console.WriteLine(" Found Astrilde");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Current = Current.nextVillage;
+                }
+            }
+
+
+        }
+    }
+
 }
 
 
